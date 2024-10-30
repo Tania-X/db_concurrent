e9e5ac29bbf4fe9c717a0991296b8def2a844f70
max <13126565350@163.com>
2024-10-30 16:34:31 +0800

    ddl

---
+++ scripts/ddl.sql
@@ -0,0 +1,6 @@
+CREATE TABLE t_order (
+                         id INT PRIMARY KEY AUTO_INCREMENT,
+                         order_no VARCHAR(50) NOT NULL,
+                         order_detail VARCHAR(255),
+                         order_flag VARCHAR(50)
+);
